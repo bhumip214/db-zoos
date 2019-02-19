d51f874d0c192c3854e88a9626e543df89963631
bhumip214 <bhumip214@gmail.com>
2019-02-19 15:22:58 -0800

    Stretch completed

--- routes/bearRoute.js
+++ routes/bearRoute.js
@@ -18,87 +18,87 @@ router.use(express.json());
 
 // endpoints here
 
-//Get all zoos from the database
+//Get all bears from the database
 router.get("/", async (req, res) => {
   try {
-    const zoos = await db("zoos");
-    res.status(200).json(zoos);
+    const bears = await db("bears");
+    res.status(200).json(bears);
   } catch (error) {
     res
       .status(500)
-      .json({ message: "The zoos information could not be retrieved." });
+      .json({ message: "The bears information could not be retrieved." });
   }
 });
 
-//Get zoo by id request
+//Get bear by id request
 router.get("/:id", async (req, res) => {
   try {
-    const zoo = await db("zoos")
+    const bear = await db("bears")
       .where({ id: req.params.id })
       .first();
-    if (zoo) {
-      res.status(200).json(zoo);
+    if (bear) {
+      res.status(200).json(bear);
     } else {
       res
         .status(404)
-        .json({ message: "The zoo with the specified ID does not exist." });
+        .json({ message: "The bear with the specified ID does not exist." });
     }
   } catch (error) {
     res
       .status(500)
-      .json({ message: "The zoo information could not be retrieved." });
+      .json({ message: "The bear information could not be retrieved." });
   }
 });
 
-// Create zoos request
+// Create bear request
 router.post("/", async (req, res) => {
   try {
     if (req.body.name) {
-      const [id] = await db("zoos").insert(req.body);
-      const zoo = await db("zoos")
+      const [id] = await db("bears").insert(req.body);
+      const bear = await db("bears")
         .where({ id })
         .first();
-      res.status(201).json(zoo);
+      res.status(201).json(bear);
     } else {
       res.status(400).json({
-        errorMessage: "Please provide the name for the zoo."
+        errorMessage: "Please provide the name for the bear."
       });
     }
   } catch (error) {
     res.status(500).json({
-      message: "There was an error while saving the zoo to the database"
+      message: "There was an error while saving the bear to the database"
     });
   }
 });
 
-// Update zoo request
+// Update bear request
 router.put("/:id", async (req, res) => {
   try {
-    const count = await db("zoos")
+    const count = await db("bears")
       .where({ id: req.params.id })
       .update(req.body);
 
     if (count > 0) {
-      const zoo = await db("zoos")
+      const bear = await db("bears")
         .where({ id: req.params.id })
         .first();
-      res.status(200).json(zoo);
+      res.status(200).json(bear);
     } else {
       res
         .status(404)
-        .json({ message: "The zoo with the specified ID does not exist." });
+        .json({ message: "The bear with the specified ID does not exist." });
     }
   } catch (error) {
     res
       .status(500)
-      .json({ message: "The zoo information could not be modified." });
+      .json({ message: "The bear information could not be modified." });
   }
 });
 
 //delete zoos request
 router.delete("/:id", async (req, res) => {
   try {
-    const count = await db("zoos")
+    const count = await db("bears")
       .where({ id: req.params.id })
       .del();
 
@@ -107,10 +107,10 @@ router.delete("/:id", async (req, res) => {
     } else {
       res
         .status(404)
-        .json({ message: "The zoo with the specified ID does not exist." });
+        .json({ message: "The bear with the specified ID does not exist." });
     }
   } catch (error) {
-    res.status(500).json({ message: "The zoo could not be deleted" });
+    res.status(500).json({ message: "The bear could not be deleted" });
   }
 });
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const zooRoute = require("./routes/zooRoute");
-
+const bearRoute = require("./routes/bearRoute");
 const server = express();
 
 server.use(express.json());
@@ -11,6 +11,7 @@ server.use(cors());
 server.use(helmet());
 server.use(morgan("dev"));
 server.use("/api/zoos", zooRoute);
+server.use("/api/bears", bearRoute);
 
 server.get("/", async (req, res, next) => {
   res.send(`<h2>Lambda Zoo API</h2>`);
