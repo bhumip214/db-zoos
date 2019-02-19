09f5726625e8f239cd3ef75aa88a05c19a2a17e7
bhumip214 <bhumip214@gmail.com>
2019-02-19 12:43:50 -0800

    Completed Get and Get by id requests

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ server.use(helmet());
 
 // endpoints here
 
+//Get all zoos from the database
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const zoos = await db("zoos");
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Get zoo by id
+server.get("/api/zoos/:id", async (req, res) => {
+  try {
+    const zoo = await db("zoos")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
